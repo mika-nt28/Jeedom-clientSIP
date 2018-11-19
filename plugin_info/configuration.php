c71e78b987836017cb7966e5b2145f170c3d6c63
Halbout Michael <michaelhalbout@gmail.com>
2018-11-19 13:53:53 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -25,3 +25,89 @@ if (!isConnect()) {
 		</fieldset>
 	</form>
 </div>
+
+ <div class="col-sm-6">
+	<form class="form-horizontal">
+		<legend>Serveur SIP</legend>
+		<fieldset>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">u-law</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="ulaw" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">a-law</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="alaw" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">GSM</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="gsm" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">ILBC</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="ilbc" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">SPEEX</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="speex" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">G.726</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="g726" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">ADPCM</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="adpcm" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">LPC10</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="lpc10" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">G.726</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="g729" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">G.723</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="g723" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">H.263</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="h263" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">H.263P</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="h263p" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-lg-4 control-label">H.264</label>
+				<div class="col-lg-4">
+					<input type="checkbox" class="configKey form-control" data-l1key="codec" data-l2key="h264" />
+				</div>
+			</div>
+		</fieldset>
+	</form>
+</div>
