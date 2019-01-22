eee98125c8e2869c833fd73a01327189fd57abf3
Halbout Michael <michaelhalbout@gmail.com>
2019-01-22 12:26:28 +0100

    Update clientSIP.ajax.php

--- core/ajax/clientSIP.ajax.php
+++ core/ajax/clientSIP.ajax.php
@@ -2,9 +2,9 @@
 try {
 	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
     	include_file('core', 'authentification', 'php');
-    	if (!isConnect('admin')) {
+    	/*if (!isConnect('admin')) {
         	throw new Exception(__('401 - Accès non autorisé', __FILE__));
-    	}
+    	}*/
 	if (init('action') == 'updateCmd') {
    		$cmd=cmd::byId(init('id'));
     		if(is_object($cmd)){
