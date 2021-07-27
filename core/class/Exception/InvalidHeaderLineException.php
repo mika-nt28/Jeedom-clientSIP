1db8f130efbd3e919ef404aa5e5eebb3ab27054f
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:10:28 +0200

    Update InvalidHeaderLineException.php

--- core/class/Exception/InvalidHeaderLineException.php
+++ core/class/Exception/InvalidHeaderLineException.php
@@ -1,12 +1,5 @@
 <?php
 /**
-* RTCKit\SIP\Exception\InvalidHeaderLineException Class
-*/
-declare(strict_types = 1);
-
-namespace RTCKit\SIP\Exception;
-
-/**
 * Exception thrown when processing SIP Messages with invalid Header Lines (RFC3261 Section 7.3)
 */
 class InvalidHeaderLineException extends SIPException
