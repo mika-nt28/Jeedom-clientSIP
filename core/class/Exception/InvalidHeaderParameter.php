bc4d0fe8ea9d59f03cd9cf3d8910f15bf37ffe26
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:10:54 +0200

    Update InvalidHeaderParameter.php

--- core/class/Exception/InvalidHeaderParameter.php
+++ core/class/Exception/InvalidHeaderParameter.php
@@ -1,10 +1,4 @@
 <?php
-/**
-* RTCKit\SIP\Exception\InvalidHeaderParameter Class
-*/
-declare(strict_types = 1);
-
-namespace RTCKit\SIP\Exception;
 
 /**
 * Exception thrown when processing SIP Messages with empty header parameters
