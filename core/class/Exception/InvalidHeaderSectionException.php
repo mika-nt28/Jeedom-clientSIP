f6d2e9cb420884b4687f3e7375ccd319f6a5b770
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:11:14 +0200

    Update InvalidHeaderSectionException.php

--- core/class/Exception/InvalidHeaderSectionException.php
+++ core/class/Exception/InvalidHeaderSectionException.php
@@ -1,12 +1,5 @@
 <?php
 /**
-* RTCKit\SIP\Exception\InvalidHeaderSectionException Class
-*/
-declare(strict_types = 1);
-
-namespace RTCKit\SIP\Exception;
-
-/**
 * Exception thrown when processing SIP Messages with incorrect Header section formatting (RFC3261 Section 7)
 */
 class InvalidHeaderSectionException extends SIPException
