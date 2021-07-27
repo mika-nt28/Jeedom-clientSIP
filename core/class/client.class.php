aca854c79f2645b225e7c1bc0f2f831c2a8085fc
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:06:35 +0200

    Create client.class.php

---
+++ core/class/client.class.php
@@ -0,0 +1,21 @@
+<php
+     
+include 'Exception\InvalidBodyLengthException';
+include 'Exception\InvalidCSeqValue';
+include 'Exception\InvalidHeaderLineException';
+include 'Exception\InvalidHeaderSectionException';
+include 'Header\CallIdHeader';
+include 'Header\ContactHeader';
+include 'Header\CSeqHeader';
+include 'Header\Header';
+include 'Header\FromHeader';
+include 'Header\MaxForwardsHeader';
+include 'Header\MultiValueHeader';
+include 'Header\MultiValueWithParamsHeader';
+include 'Header\NameAddrHeader';
+include 'Header\RAckHeader';
+include 'Header\RSeqHeader';
+include 'Header\ScalarHeader';
+include 'Header\SingleValueWithParamsHeader';
+include 'Header\ViaHeader';
+?>
