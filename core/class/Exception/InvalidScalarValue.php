83fb00fd330a74876446013b05f6f59bedf8a9ef
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:13:37 +0200

    Update InvalidScalarValue.php

--- core/class/Exception/InvalidScalarValue.php
+++ core/class/Exception/InvalidScalarValue.php
@@ -1,10 +1,4 @@
 <?php
-/**
-* RTCKit\SIP\Exception\InvalidScalarValue Class
-*/
-declare(strict_types = 1);
-
-namespace RTCKit\SIP\Exception;
 
 /**
 * Exception thrown when processing SIP messages with scalar values out of bounds
