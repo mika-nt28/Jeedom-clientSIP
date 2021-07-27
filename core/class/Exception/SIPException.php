5be65ccb9e411e339d12b1dacf2baedfcf293ce3
Halbout Michael <michaelhalbout@gmail.com>
2021-07-27 18:12:21 +0200

    Update SIPException.php

--- core/class/Exception/SIPException.php
+++ core/class/Exception/SIPException.php
@@ -1,13 +1,4 @@
 <?php
-/**
-* RTCKit\SIP\Exception\SIPException Class
-*/
-declare(strict_types = 1);
-
-namespace RTCKit\SIP\Exception;
-
-use DomainException;
-use Throwable;
 
 /**
 * Generic SIP exception
