8fe814c515619f82109479f3883d4f1fe1559f00
Halbout Michael <michaelhalbout@gmail.com>
2021-07-28 09:43:56 +0200

    Update client.class.php

--- core/class/client.class.php
+++ core/class/client.class.php
@@ -1,106 +1,90 @@
 <?php
      
-include 'Exception\InvalidBodyLengthException';
-include 'Exception\InvalidCSeqValue';
-include 'Exception\InvalidHeaderLineException';
-include 'Exception\InvalidHeaderSectionException';
-include 'Header\CallIdHeader';
-include 'Header\ContactHeader';
-include 'Header\CSeqHeader';
-include 'Header\Header';
-include 'Header\FromHeader';
-include 'Header\MaxForwardsHeader';
-include 'Header\MultiValueHeader';
-include 'Header\MultiValueWithParamsHeader';
-include 'Header\NameAddrHeader';
-include 'Header\RAckHeader';
-include 'Header\RSeqHeader';
-include 'Header\ScalarHeader';
-include 'Header\SingleValueWithParamsHeader';
-include 'Header\ViaHeader';
-class sip{ 
-     public function __construct($jeedom = null,$src_ip = null, $src_port = null, $fr_timer = null, $socket_bind = true)	{
-		$this->jeedom = $jeedom;
-		$this->socket_bind = $socket_bind;
-		if (!function_exists('socket_create')){
-			log::add('clientSIP','error',$this->jeedom->getHumanName()." socket_create() function missing.");
-			die();
-		}
-		if ($src_ip){
-			if (!preg_match('/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/', $src_ip)){
-				log::add('clientSIP','error',$this->jeedom->getHumanName()." Invalid src_ip $src_ip");
-				die();
-			}
-		}else{
-			if (isset($_SERVER['SERVER_ADDR'])){
-				// running in a web server
-				$src_ip = $_SERVER['SERVER_ADDR'];
-			}else	{
-				// running from command line
-				$addr = gethostbynamel(php_uname('n'));
-				if (!is_array($addr) || !isset($addr[0]) || substr($addr[0],0,3) == '127')	{
-					log::add('clientSIP','error',$this->jeedom->getHumanName()." Failed to obtain IP address to bind. Please set bind address manualy.");
-					die();
-				}
-				$src_ip = $addr[0];
-			}
-		}
-		$this->src_ip = $src_ip;
-		if ($src_port){
-			if (!preg_match('/^[0-9]+$/',$src_port)){
-				log::add('clientSIP','error',$this->jeedom->getHumanName(). " Invalid src_port $src_port");
-				die();
-			}
-			$this->src_port = $src_port;
-			$this->lock_file = null;
-		}
-		if ($fr_timer){
-			if (!preg_match('/^[0-9]+$/',$fr_timer)){
-				log::add('clientSIP','error',$this->jeedom->getHumanName()."Invalid fr_timer $fr_timer");
-				die();
-			}
-			$this->fr_timer = $fr_timer;
-		}
-		$this->createSocket();
+include 'Request.php';
+include 'Header/Header.php';
+
+class client{ 
+     public function __construct($src_ip = null, $src_port = null ,$CallNumber,$userAgent, $socket_bind = true)	{
+		$this->_cHost = $src_ip;
+		$this->_cPort = $src_port;
+	  	$this->_CallNumber = $CallNumber;
+	  	$this->_userAgent = $userAgent;
+		$this->_sHost=config::byKey('Host', 'clientSIP');
+		$this->_sPort=config::byKey('Port', 'clientSIP');
+		$this->createSocket($socket_bind);
 	}
      public function __destruct(){
 		$this->closeSocket();
 	}
-     private function createSocket(){ 
-		$this->getPort();
-		if (!$this->src_ip){
-			log::add('clientSIP','error',$this->jeedom->getHumanName()."Source IP not defined.");
-			die();
-		}
+     private function createSocket($socket_bind){ 
 		if (!$this->socket = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)){
 			$err_no = socket_last_error($this->socket);
-			log::add('clientSIP','error',$this->jeedom->getHumanName().socket_strerror($err_no));
+			log::add('clientSIP','error',socket_strerror($err_no));
 			die();
 		}
-		if($this->socket_bind){
-			if (!@socket_bind($this->socket, $this->src_ip, $this->src_port)){
+		if($socket_bind){
+			if (!@socket_bind($this->socket, $this->_cHost, $this->_cPort)){
 				$err_no = socket_last_error($this->socket);
-				log::add('clientSIP','error',$this->jeedom->getHumanName()."Failed to bind ".$this->src_ip.":".$this->src_port." ".socket_strerror($err_no));
+				log::add('clientSIP','error',"Failed to bind ".$this->_cHost.":".$this->_cPort." ".socket_strerror($err_no));
 				die();
 			}
 		}
-		$microseconds = $this->fr_timer * 1000;
-		$usec = $microseconds % 1000000;
-		$sec = floor($microseconds / 1000000);
-		if (!@socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>$sec,"usec"=>$usec))){
+		if (!@socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>5,"usec"=>0))){
 			$err_no = socket_last_error($this->socket);
-			log::add('clientSIP','error',$this->jeedom->getHumanName().socket_strerror($err_no));
+			log::add('clientSIP','error',socket_strerror($err_no));
 			die();
 		}
 		if (!@socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, array("sec"=>5,"usec"=>0))){
 			$err_no = socket_last_error($this->socket);
-			log::add('clientSIP','error',$this->jeedom->getHumanName().socket_strerror($err_no));
+			log::add('clientSIP','error',socket_strerror($err_no));
 			die();
 		}
 	}
 	private function closeSocket(){
 		socket_close($this->socket);
-		$this->releasePort();
+	}
+	public function register(){
+		$request = new Request;
+		$request->version = 'SIP/2.0';
+		$request->method = 'REGISTER';
+		$request->uri = 'sip:'.$this->_cHost;
+
+		$request->via = new ViaHeader;
+		$request->via->values[0] = new ViaValue;
+		$request->via->values[0]->protocol = 'SIP';
+		$request->via->values[0]->version = '2.0';
+		$request->via->values[0]->transport = 'UDP';
+		$request->via->values[0]->host = .$this->_sHost.':'.$this->_sPort;
+		$request->via->values[0]->branch = 'z9hG4bK.eAV4o0nXr';
+
+		$request->from = new NameAddrHeader;
+		$request->from->addr = 'sip:'.$this->_CallNumber.'@'.$this->_cHost.':'.$this->_sPort;
+		$request->from->tag = 'SFJbQ2oWh';
+
+		//$request->to = new NameAddrHeader;
+		//$request->to->addr = 'sip:'.$this->_CallNumber.'@'.$this->_cHost.':'.$this->_cPort;
+
+		$request->cSeq = new CSeqHeader;
+		$request->cSeq->sequence = 20;
+		$request->cSeq->method = $request->method;
+
+		$request->callId = new CallIdHeader;
+		$request->callId->value = 'ob0EYyuyC0';
+
+		$request->maxForwards = new ScalarHeader;
+		$request->maxForwards->value = 70;
+
+		$request->contact = new ContactHeader;
+		$request->contact->values[0] = new ContactValue;
+		$request->contact->values[0]->addr = 'sip:'.$this->_CallNumber.'@'.$this->_cHost.':'.$this->_cPort.';transport=udp';
+		$request->contact->values[0]->q = 0.7;
+		$request->contact->values[0]->expires = 3600;
+		//$request->contact->values[0]->params['+sip.instance'] = '"<urn:uuid:5cc54b96-ab90-4652-b4e5-de74c8e56fb7>"';
+
+		$request->userAgent = new Header;
+		$request->userAgent->values[0] = $this->_userAgent;
+
+		$request->render();
 	}
 }
 ?>
