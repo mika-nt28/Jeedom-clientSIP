69e5423e6488ad19f1140479de2838028618c958
Halbout Michael <michaelhalbout@gmail.com>
2021-07-29 09:26:35 +0200

    Update clientSIP.js

--- desktop/js/clientSIP.js
+++ desktop/js/clientSIP.js
@@ -1,3 +1,11 @@
+$('.eqLogicAction[data-action=gotoMoniteur]').off().on('click', function () {	
+  	bootbox.dialog({
+		title: "{{Moniteur}}",
+		size: "large",
+		message: $('<div>').load('index.php?v=d&modal=clientSIP.moniteur&plugin=clientSIP&type=clientSIP'),
+		
+	});
+});
 function addCmdToTable(_cmd) {
 	if (!isset(_cmd)) {
 		var _cmd = {configuration: {}};
