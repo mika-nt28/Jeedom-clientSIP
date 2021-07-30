e90a58510c5c5e48a0ff20e86abd7df7d24ff61f
Halbout Michael <michaelhalbout@gmail.com>
2021-07-30 10:47:15 +0200

    Create Body.php

---
+++ core/class/Body/Body.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* Body Class
+*/
+class Body
+{
+    /** @var list<string> Generic body field values */
+    public $values = [];
+
+    final public function __construct() {}
+
+    /**
+     * Generic header value parser
+     *
+     * @param list<string> $body Body body
+     * @return Header
+     */
+    public static function parse(array $body): Body
+    {
+        $ret = new static;
+
+        foreach ($body as $line) {
+            $ret->values[] = trim($line);
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Generic body value renderer
+     *
+     * @param string $bname Body field name
+     * @return string
+     */
+    public function render(string $bname): string
+    {
+        $ret = '';
+
+        foreach ($this->values as $value) {
+            $ret .= "{$bname}: {$value}\r\n";
+        }
+
+        return $ret;
+    }
+}
