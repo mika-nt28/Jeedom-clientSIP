42303037dc2a03e163c7c7e119e7cd4b7abe12aa
Halbout Michael <michaelhalbout@gmail.com>
2021-08-02 19:52:10 +0200

    Update clientSIP.class.php

--- core/class/clientSIP.class.php
+++ core/class/clientSIP.class.php
@@ -135,15 +135,15 @@ class clientSIP extends eqLogic {
 				switch($clientSIP->_sip->listen()->request){
 					case 'INVITE':
 						sleep(1);
-						$clientSIP->_sip->reply(100,'Ok');
+						$clientSIP->_sip->reply(100)
 						sleep(1);
-						$clientSIP->_sip->reply(180,'Ok');
+						$clientSIP->_sip->reply(180);
 						sleep(1);
-						$clientSIP->_sip->reply(200,'Ok');
+						$clientSIP->_sip->reply(200);
 						sleep(1);
-						$clientSIP->Decrocher();
-                        			$clientSIP->calling();
-                       				$clientSIP->Racrocher();
+						$clientSIP->checkAndUpdateCmd('CallStatus','Appel en cours');
+						$clientSIP->calling();
+						$clientSIP->Racrocher();
 					break;
 					case 'MESSAGE':
 						message::add('sucess', $clientSIP->_sip->getBody());
@@ -217,16 +217,13 @@ class clientSIP extends eqLogic {
 		else
 			$this->checkAndUpdateCmd('RegStatus','Echec');	*/		
 	}
-	public function Decrocher() {
-		if($this->_sip == null)			
-			$this->CreateConnexion(false);
-		$this->checkAndUpdateCmd('CallStatus','Appel en cours');
-		sleep(5);
-	}
 	public function Racrocher() {
 		if($this->_sip == null)			
 			$this->CreateConnexion(false);
 		$CallStatus=$this->getCmd(null,'CallStatus');
+		if ($this->_sip->request('BYE')->code == '200')		
+			$this->checkAndUpdateCmd('CallStatus','Racrocher');
+
 	/*	if($CallStatus->execCmd() == 'Sonnerie'){
 			$this->_sip->reply(487,'Request Terminated');
 			$this->_sip->reply(603,'Decline');
@@ -245,10 +242,11 @@ class clientSIP extends eqLogic {
 		$this->checkAndUpdateCmd('CallStatus','Racrocher');
 		if($this->_sip == null)			
 			$this->CreateConnexion();
+			$this->checkAndUpdateCmd('CallStatus','Sonnerie');
 		if ($this->_sip->newCall($number)->code == '200')
 			$this->checkAndUpdateCmd('CallStatus','Appel en cours');
 		else
-			$this->checkAndUpdateCmd('CallStatus','Echec');
+			$this->checkAndUpdateCmd('CallStatus','Racrocher');
 		/*$this->_sip->setUsername($this->_Username);
 		$this->_sip->setPassword($this->_Password);
 		$this->_sip->newCall();
@@ -259,29 +257,27 @@ class clientSIP extends eqLogic {
 		$this->_sip->setMethod('INVITE');
 		$res=$this->_sip->send();*/
 		sleep(5);
-		//$this->checkAndUpdateCmd('CallStatus','Appel en cours');
-		$this->Decrocher();
 		$this->calling();
 		$this->Racrocher();
 	}
 	public function sendMessage($number,$message) {	
 		log::add('clientSIP', 'debug', 'Envoie un message => ' . $number);
-		$this->checkAndUpdateCmd('CallStatus','Racrocher');	
+		/*$this->checkAndUpdateCmd('CallStatus','Racrocher');	
 		$this->CreateConnexion();
 		$this->_sip->setUsername($this->_Username);
 		$this->_sip->setPassword($this->_Password);
 		$this->_sip->newCall();
 		$this->_sip->setFrom('sip:'.$this->_CallNumber.'@'.$this->_Host.':'.$this->_Port);
-		$this->_sip->setUri('sip:'.$number.'@'.$this->_Host.':'.$this->_Port/*.';transport='.$this->getConfiguration("transport")*/);
+		$this->_sip->setUri('sip:'.$number.'@'.$this->_Host.':'.$this->_Port);
 		$this->_sip->setTo('sip:'.$number.'@'.$this->_Host.':'.$this->_Port);
 		$this->_sip->setMethod('MESSAGE');
-		$res=$this->_sip->send();
+		$res=$this->_sip->send();*/
 	}
 	public function calling(){
 		$cmd ='ffmpeg -i '.$this->TextToSpeach("Test de communication de Jeedom sur une communication sip").$this->_sip->getFFMEGcodec().$this->_sip->getRtsp();
 		$cmd .= ' >> ' . log::getPathToLog('clientSIP');// . ' 2>&1';
 		exec($cmd);
-    }
+	}
 	public function AddCommande($Name,$_logicalId,$Type="info", $SubType='string',$Template='default') {
 		$Commande = $this->getCmd(null,$_logicalId);
 		if (!is_object($Commande)){
