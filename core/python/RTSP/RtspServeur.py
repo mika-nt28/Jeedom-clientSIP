be08bdadad49976d0fd9fcfae61b691574a3a4fd
Halbout Michael <michaelhalbout@gmail.com>
2022-04-29 10:30:05 +0200

    Create RtspServeur.py

---
+++ core/python/RTSP/RtspServeur.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+from socket import *
+
+class RtspServeur():
+  def __init__(self,port):
+    self.addr = ('0.0.0.0 ', port)
+    self.socket = socket(af_inet, sock_dgram)
+    self.socket.bind(self.addr)
+    self.socket.Setblocking(0)
+  def stream(self):
+    while True:
+      data = None
+      try:
+        data, _ = self.socket.recvfrom(921600)
+        receive_data = np.frombuffer(data, dtype='uint8')
+        #Analyse DMTF
+        #Analyse SpeachToText
+      except BlockingIOError as e:
+        pass
