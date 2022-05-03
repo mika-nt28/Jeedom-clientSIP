dbb518dac63f5ed7110dd63bc86f3f9c4ba9ace2
Halbout Michael <michaelhalbout@gmail.com>
2022-05-03 10:41:49 +0200

    Update RtspServeur.py

--- core/python/RTSP/RtspServeur.py
+++ core/python/RTSP/RtspServeur.py
@@ -1,17 +1,16 @@
 import numpy as np
-import cv2
 from socket import *
 
 class RtspServeur():
-  def __init__(self,port):
-    self.addr = ('0.0.0.0 ', port)
-    self.socket = socket(af_inet, sock_dgram)
-    self.socket.bind(self.addr)
-    self.socket.Setblocking(0)
-  def stream(self):
-    data = None
-    try:
-      data, _ = self.socket.recvfrom(921600)
-      return np.frombuffer(data, dtype='uint8')
-    except BlockingIOError as e:
-      pass
+	def __init__(self,port):
+		self.addr = ('0.0.0.0 ', port)
+		self.socket = socket(af_inet, sock_dgram)
+		self.socket.bind(self.addr)
+		self.socket.Setblocking(0)
+	def stream(self):
+		data = None
+		try:
+			data, _ = self.socket.recvfrom(921600)
+			return np.frombuffer(data, dtype='uint8')
+		except BlockingIOError as e:
+			pass
