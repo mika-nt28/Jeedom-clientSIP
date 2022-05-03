19d3f859c80eb97d3335df0acccf05060a3bf719
Halbout Michael <michaelhalbout@gmail.com>
2022-05-03 13:38:28 +0200

    Update RtspClient.py

--- core/python/RTSP/RtspClient.py
+++ core/python/RTSP/RtspClient.py
@@ -1,10 +1,10 @@
 import cv2
-from socket import *
+import socket
 
 class RtspClient():
 	def __init__(self,url,port):
 		self.addr = (url, port)
-		self.socket = socket(af_inet, sock_dgram) # create UDP socket
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	def stream(self,data):
 		self.socket.sendto(data,self.addr)
 	def streamMessage(self,message):
@@ -17,4 +17,4 @@ class RtspClient():
 			_, img = cap.read()
 			img = cv2.flip(img, 1)
 			_, send_data = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 50])
-			self.stream(send_data):
+			self.stream(send_data)
