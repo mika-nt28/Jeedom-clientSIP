fb1f61999aa98a7080db08211036dcc15598a49a
Halbout Michael <michaelhalbout@gmail.com>
2022-08-03 15:33:43 +0200

    Update RtspClient.py

--- core/python/RTSP/RtspClient.py
+++ core/python/RTSP/RtspClient.py
@@ -1,20 +1,14 @@
-import cv2
-import socket
-
+import sys
+from tkinter import Tk
+from ClientWorker import ClientWorker
 class RtspClient():
-	def __init__(self,url,port):
-		self.addr = (url, port)
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	def stream(self,data):
-		self.socket.sendto(data,self.addr)
-	def streamMessage(self,message):
-		pass
-	def streamCamera(self,camera=0):
-		cap = cv2.VideoCapture(camera)
-		cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-		cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-		while True:
-			_, img = cap.read()
-			img = cv2.flip(img, 1)
-			_, send_data = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 50])
-			self.stream(send_data)
+	def __init__(self,serverAddr,rtpPort):
+		self.serverAddr = serverAddr
+		self.serverPort = serverPort
+		self.rtpPort = rtpPort
+	def stream(self,fileName):
+		root = Tk()
+		# Create a new client
+		app = Client(root, self.serverAddr, self.serverPort, self.rtpPort, fileName)
+		app.master.title("RTPClient")	
+		root.mainloop()
