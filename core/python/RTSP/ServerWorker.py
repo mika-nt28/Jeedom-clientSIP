d7646a80569c8983dc272c5d616a180ddcb388b7
Halbout Michael <michaelhalbout@gmail.com>
2022-08-03 15:26:41 +0200

    Update ServerWorker.py

--- core/python/RTSP/ServerWorker.py
+++ core/python/RTSP/ServerWorker.py
@@ -21,6 +21,28 @@ class ServerWorker:
 	
 	clientInfo = {}
 	
+	DTMF_TABLE = {
+		'1': [1209, 697],
+		'2': [1336, 697],
+		'3': [1477, 697],
+		'A': [1633, 697],
+
+		'4': [1209, 770],
+		'5': [1336, 770],
+		'6': [1477, 770],
+		'B': [1633, 770],
+
+		'7': [1209, 852],
+		'8': [1336, 852],
+		'9': [1477, 852],
+		'C': [1633, 852],
+
+		'*': [1209, 941],
+		'0': [1336, 941],
+		'#': [1477, 941],
+		'D': [1633, 941]
+	} 
+	
 	def __init__(self, clientInfo):
 		self.clientInfo = clientInfo
 		
@@ -159,3 +181,23 @@ class ServerWorker:
 			print ("404 NOT FOUND")
 		elif code == self.CON_ERR_500:
 			print ("500 CONNECTION ERROR")
+	def DTMF_detetor(self,data):
+		if len(data) == 0:
+			return None
+		# Calculate fourier trasform of data
+		FourierTransformOfData = np.fft.fft(data, 20000)
+		# Convert fourier transform complex number to integer numbers
+		for i in range(len(FourierTransformOfData)):
+			FourierTransformOfData[i] = int(np.absolute(FourierTransformOfData[i]))
+			# Calculate lower bound for filtering fourier trasform numbers
+			LowerBound = 20 * np.average(FourierTransformOfData)
+			# Filter fourier transform data (only select frequencies that X(jw) is greater than LowerBound)
+			FilteredFrequencies = []
+			for i in range(len(FourierTransformOfData)):
+				if (FourierTransformOfData[i] > LowerBound):
+					FilteredFrequencies.append(i)
+					# Detect and print pressed button
+					for char, frequency_pair in DTMF_TABLE.items():
+						if (isNumberInArray(FilteredFrequencies, frequency_pair[0]) and isNumberInArray(FilteredFrequencies, frequency_pair[1])):
+							return char
+		return None
